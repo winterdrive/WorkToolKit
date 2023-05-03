dc46bd59c9e736ae452a3cffa960eab74c8d109b
winterdrive <90021888+winterdrive@users.noreply.github.com>
2023-05-03 16:14:01 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /home/sktang/project/serverAlert/
 gunicorn3 --workers=1 app:app -b 0.0.0.0:7998 --daemon --reload
+cd /home/sktang/project/lnbAlert/
+gunicorn3 --workers=1 app:app -b 0.0.0.0:7997 --daemon --reload
